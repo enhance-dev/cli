3b2aff03d732fb1bf4074e589729e4a31d5c6010
macdonst <simon.macdonald@gmail.com>
2023-03-31 13:28:41 -0400

    Update help commands
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/commands/help/index.js
+++ src/commands/help/index.js
@@ -21,69 +21,11 @@ let help = {
         ],
       },
       {
-        header: 'Manage Enhance apps &\nenvironments',
-        items: [
-          {
-            name: 'builds',
-            description: 'View build logs from app deployments',
-          },
-          {
-            name: 'create',
-            description: 'Create a Enhance app or environment',
-          },
-          {
-            name: 'deploy',
-            description: 'Deploy an app to Enhance',
-          },
-          {
-            name: 'destroy',
-            description: 'Destroy a Enhance app or environment',
-          },
-          {
-            name: 'env',
-            description: `List your Enhance app's environment variables`,
-          },
-          {
-            name: 'list',
-            description: 'List your Enhance apps and environments',
-          },
-          {
-            name: 'login',
-            description: 'Log into Enhance',
-          },
-          {
-            name: 'logout',
-            description: 'Log out of Enhance',
-          },
-          {
-            name: 'logs',
-            description: 'Retrieve logs from Enhance',
-          },
-          {
-            name: 'tail',
-            description: 'Tail logs from Enhance',
-          },
-        ],
-      },
-      {
-        header: 'Manage your Enhance account domains',
-        items: [
-          {
-            name: 'domains',
-            description: 'List your Enhance account domain names',
-          },
-        ],
-      },
-      {
         header: 'Other',
         items: [
           {
             name: 'help',
             description: 'Display help',
-          },
-          {
-            name: 'telemetry',
-            description: 'Enable or disable basic CLI telemetry',
           },
           {
             name: 'update',
