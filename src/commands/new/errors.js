e42e59b8699a508afe15216eb0f4b861974103ea
macdonst <simon.macdonald@gmail.com>
2023-11-09 16:01:44 -0500

    Better error message
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/commands/new/errors.js
+++ src/commands/new/errors.js
@@ -5,6 +5,7 @@ module.exports = function error (params, utils) {
     let errors = {
       en: {
         no_path: 'Project path not found, please run with -p or --path',
+        path_exists: 'Destination path already exists',
         project_found: 'Existing Enhance app already found in this directory',
         invalid_appname: `Invalid app name`,
         invalid_runtime: `Function runtime must be one of: ${backtickify(runtimes)}`,
